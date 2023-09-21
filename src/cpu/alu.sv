c027996b43cc8bc9852b9fe3acc5a0f81abc33f5
calexanderberg <carlalexanderberg@gmail.com>
2023-09-21 19:52:06 +0200

    Add: finished R type woth RV32M extension

--- src/cpu/alu.sv
+++ src/cpu/alu.sv
@@ -33,55 +33,53 @@ module r_type (
 case (funct3)
   3'h0: // ADD | SUB || MUL
     case (funct7)
-      6'h00: rd <= rs1 + rs2;          // ADD
-      6'h20: rd <= rs1 - rs2;          // SUB
-      6'h01: rd <= (rs1 * rs2)[31:0];  // MUL
+      6'h00: rd <= rs1 + rs2                      // ADD
+      6'h20: rd <= rs1 - rs2;                     // SUB
+      6'h01: rd <= ($signed(rs1) * $signed(rs2);  // MUL
     endcase
 
   3'h1: // SLL | MULH
     case (funct7)
-      6'h00: rd <= rs1 <<  rs2;        // SLL
-      6'h01: rd <= (rs1 * rs2)[63:32]; // MULM
+      6'h00: rd <= rs1 <<  rs2;                          // SLL
+      6'h01: rd = (($signed(rs1) * $signed(rs2)) >> 32); // MULH
     endcase
 
   3'h2: // SLT | MUILSU
     case (funct7)
-      6'h00: rd <= (rs1 <  rs2);        // SLT
-      6'h01: rd <= (rs1 * rs2)[63:32];  // FIX
+      6'h00: rd <= (rs1 <  rs2);                           // SLT
+      6'h01: rd <= (($signed(rs1) * $signed(rs2)) >> 32);  // MULSU
     endcase
 
   3'h3: // SLTU MULU
     case (funct7)
-      6'h00: rd <= (rs1 <0 rs2);        // SLTU
-      6'h01: rd <= (rs1 * rs2)[63:32];  // FIX
+      6'h00: rd <= (rs1 <0 rs2);         // SLTU
+      6'h01: rd <= ((rs1 * rs2) >> 32);  // MULU
     endcase
 
   3'h4: // XOR | DIV
     case (funct7)
-      6'h00: rrd <= rs1 ^ rs2;          // XOR
-      6'h01: rd <= (rs1 * rs2)[63:32];  // FIX
+      6'h00: rrd <= rs1 ^ rs2;                   // XOR
+      6'h01: rd <= ($signed(rs1) / $signed(rs2); // DIV
     endcase
 
   3'h5: // SRL | SRA | DIVU
     case (funct7)
       6'h00: rd <= rs1 >> rs2;          // SRL
       6'h20: rd <= rs1 >>> rs2;         // SRA
-      6'h01: rd <= (rs1 * rs2)[63:32];  // FIX
+      6'h01: rd <= rs1 / rs2;           // DIVU
     endcase
 
   3'h6: // OR | REM
     case (funct7)
-      6'h00: rd <= rs1 |  rs2;        // OR
-      6'h01: rd <= (rs1 * rs2)[63:32];  // FIX
+      6'h00: rd <= rs1 |  rs2;                     // OR
+      6'h01: rd <= ($signed(rs1) % $signed(rs2));  // REM
     endcase
-
 
   3'h7: // AND | REMU
     case (funct7)
-      6'h00: rd <= rs1 & rs2;        // AND
-      6'h01: rd <= (rs1 * rs2)[63:32];  // FIX
+      6'h00: rd <= rs1 & rs2;            // AND
+      6'h01: rd <= (rs1 * rs2)[63:32];  // REM
     endcase
-
 endcase
 endmodule
 
