672cffb7114e2c94ce090a62b7ceb1a0736b38cd
21st <60197499+21st-centuryman@users.noreply.github.com>
2023-09-21 17:45:54 +0200

    Add: boilerplate

--- tests/alu_tb.sv
+++ tests/alu_tb.sv
@@ -9,35 +9,40 @@ module TB();
   initial begin
     op = 7'b0110011 // Will not change
     
-    f3 = 3'h0; f7 = 6'h00 // ADD
+    f3 = 3'h0; f7 = 6'h00; // ADD
     a = 4; b = 6;
     out1 = (o == 10);
     a = -2; b = -4;
     out2 = (o == -6);
     $display("ADD: ", out1, " ", out2 );
-
-    f3 = 3'h0; f7 = 6'h20 // SUB
+    
+    f3 = 3'h0; f7 = 6'h20; // SUB
     a = 4; b = 6;
     out1 = (o == -2);
     a = -2; b = -4;
     out2 = (o == 2);
-    $display("ADD: ", out1, " ", out2 );
-
-    /*
-    f3 = 3'h0; f7 = 6'h00 // ADD
+    $display("SUB: ", out1, " ", out2 );
+    
+    f3 = 3'h0; f7 = 6'h01; // MUL
     a = 4; b = 6;
-    out1 = (o == 10);
+    out1 = (o == 24);
     a = -2; b = -4;
-    out2 = (o == -6);
-    $display("ADD: ", out1, " ", out2 );
-
-    f3 = 3'h0; f7 = 6'h00 // ADD
+    out2 = (o == 8);
+    $display("MUL: ", out1, " ", out2 );
+    
+    f3 = 3'h1; f7 = 6'h00; // SLL
+    a = 4; b = 2;
+    out1 = (o == 16);
+    a = -2; b = 3;
+    out2 = (o == -16);
+    $display("SLL: ", out1, " ", out2 );
+    
+    f3 = 3'h1; f7 = 6'h01; // MULH
     a = 4; b = 6;
-    out1 = (o == 10);
+    out1 = (o == 0); // Since MULH only takes the upper 32 bits of the result
     a = -2; b = -4;
-    out2 = (o == -6);
-    $display("ADD: ", out1, " ", out2 );
-    */
+    out2 = (o == 0);
+    $display("MULH: ", out1, " ", out2 );
   end
 
   /* Edit to make it work for I and other types
