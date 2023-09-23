d273585cd2d0f743a98a12e4ac523196ba0eff52
calexanderberg <carlalexanderberg@gmail.com>
2023-09-23 08:58:04 +0200

    Add: working test and alu, some bugs to fix later

--- tests/cpu/alu_tb.sv
+++ tests/cpu/alu_tb.sv
@@ -1,9 +1,11 @@
 `timescale 10ns/1ns
 
 module TB();
-  // ADD INPUTS
+  logic[3:0] f3;
+  logic[6:0] f7;
+  logic[31:0] a, b, o;
+  logic out1, out2;
 
-  /* --------------------------------- R TYPE -------------------------------- */
   alu alu(.funct7(f7), .rs2(b), .rs1(a), .funct3(f3), .rd(o)); 
 
   initial begin
@@ -11,147 +13,183 @@ module TB();
       $dumpvars(0, alu, b, a, f3, f7, o);
  
       // ADD
-      funct3 = 3'h0; funct7 = 6'h00;
+      f3 = 3'h0; f7 = 6'h00;
       a = 4; b = 6;
-      out1 = (rd == 10);
+      #1
+      out1 = (o == 10);
       a = -2; b = -4;
-      out2 = (rd == -6);
-      $display("ADD: %s %s", out1 ? "✔" : "X", out2 ? "✔" : "X");
+      #1
+      out2 = (o == -6);
+      $display("ADD: %s %s", out1 ? "W" : "X", out2 ? "W" : "X");
 
       // SUB
-      funct3 = 3'h0; funct7 = 6'h20;
+      f3 = 3'h0; f7 = 6'h20;
       a = 4; b = 6;
-      out1 = (rd == -2);
+      #1
+      out1 = (o == -2);
       a = -2; b = -4;
-      out2 = (rd == 2);
-      $display("SUB: %s %s", out1 ? "✔" : "X", out2 ? "✔" : "X");
+      #1
+      out2 = (o == 2);
+      $display("SUB: %s %s", out1 ? "W" : "X", out2 ? "W" : "X");
 
       // MUL
-      funct3 = 3'h0; funct7 = 6'h01;
+      f3 = 3'h0; f7 = 6'h01;
       a = 4; b = 6;
-      out1 = (rd == 24);
+      #1
+      out1 = (o == 24);
       a = -2; b = -4;
-      out2 = (rd == 8);
-      $display("MUL: %s %s", out1 ? "✔" : "X", out2 ? "✔" : "X");
+      #1
+      out2 = (o == 8);
+      $display("MUL: %s %s", out1 ? "W" : "X", out2 ? "W" : "X");
 
       // SLL
-      funct3 = 3'h1; funct7 = 6'h00;
+      f3 = 3'h1; f7 = 6'h00;
       a = 4; b = 2;
-      out1 = (rd == 16);
+      #1
+      out1 = (o == 16);
       a = -2; b = 3;
-      out2 = (rd == -16);
-      $display("SLL: %s %s", out1 ? "✔" : "X", out2 ? "✔" : "X");
+      #1
+      out2 = (o == -16);
+      $display("SLL: %s %s", out1 ? "W" : "X", out2 ? "W" : "X");
 
       // MULH
-      funct3 = 3'h1; funct7 = 6'h01;
+      f3 = 3'h1; f7 = 6'h01;
       a = 4; b = 6;
-      out1 = (rd == 0); // Assuming the result should be 0
+      #1
+      out1 = (o == 0); // Assuming the result should be 0
       a = -2; b = -4;
-      out2 = (rd == 0); // Assuming the result should be 0
-      $display("MULH: %s %s", out1 ? "✔" : "X", out2 ? "✔" : "X");
+      #1
+      out2 = (o == 0); // Assuming the result should be 0
+      $display("MULH: %s %s", out1 ? "W" : "X", out2 ? "W" : "X");
 
       // SLT
-      funct3 = 3'h2; funct7 = 6'h00;
+      f3 = 3'h2; f7 = 6'h00;
       a = 4; b = 6;
-      out1 = (rd == 1);
+      #1
+      out1 = (o == 1);
       a = -2; b = -4;
-      out2 = (rd == 0);
-      $display("SLT: %s %s", out1 ? "✔" : "X", out2 ? "✔" : "X");
+      #1
+      out2 = (o == 0);
+      $display("SLT: %s %s", out1 ? "W" : "X", out2 ? "W" : "X");
 
       // Test MULSU operation
-      funct3 = 3'h2; funct7 = 6'h01; // MULSU
+      f3 = 3'h2; f7 = 6'h01; // MULSU
       a = 4; b = 6;
-      out1 = (rd == 0); // Assuming the result should be 0
+      #1
+      out1 = (o == 0); // Assuming the result should be 0
       a = -2; b = -4;
-      out2 = (rd == 0); // Assuming the result should be 0
-      $display("MULSU: %s %s", out1 ? "✔" : "X", out2 ? "✔" : "X");
+      #1
+      out2 = (o == 0); // Assuming the result should be 0
+      $display("MULSU: %s %s", out1 ? "W" : "X", out2 ? "W" : "X");
 
       // SLTU 
-      funct3 = 3'h3; funct7 = 6'h00; 
+      f3 = 3'h3; f7 = 6'h00; 
       a = 4; b = 6;
-      out1 = (rd == 1);
+      #1
+      out1 = (o == 1);
       a = -2; b = -4;
-      out2 = (rd == 0);
-      $display("SLTU: %s %s", out1 ? "✔" : "X", out2 ? "✔" : "X");
+      #1
+      out2 = (o == 0);
+      $display("SLTU: %s %s", out1 ? "W" : "X", out2 ? "W" : "X");
 
       // MULU
-      funct3 = 3'h3; funct7 = 6'h01;
+      f3 = 3'h3; f7 = 6'h01;
       a = 4; b = 6;
-      out1 = (rd == 0); // Assuming the result should be 0
+      #1
+      out1 = (o == 0); // Assuming the result should be 0
       a = -2; b = -4;
-      out2 = (rd == 0); // Assuming the result should be 0
-      $display("MULU: %s %s", out1 ? "✔" : "X", out2 ? "✔" : "X");
+      #1
+      out2 = (o == 0); // Assuming the result should be 0
+      $display("MULU: %s %s", out1 ? "W" : "X", out2 ? "W" : "X");
 
       // XOR
-      funct3 = 3'h4; funct7 = 6'h00;
+      f3 = 3'h4; f7 = 6'h00;
       a = 4; b = 6;
-      out1 = (rd == 2);
+      #1
+      out1 = (o == 2);
       a = -2; b = -4;
-      out2 = (rd == -2);
-      $display("XOR: %s %s", out1 ? "✔" : "X", out2 ? "✔" : "X");
+      #1
+      out2 = (o == -2);
+      $display("XOR: %s %s", out1 ? "W" : "X", out2 ? "W" : "X");
 
       // DIV
-      funct3 = 3'h4; funct7 = 6'h01;
+      f3 = 3'h4; f7 = 6'h01;
       a = 8; b = 2;
-      out1 = (rd == 4);
+      #1
+      out1 = (o == 4);
       a = -8; b = 2;
-      out2 = (rd == -4);
-      $display("DIV: %s %s", out1 ? "✔" : "X", out2 ? "✔" : "X");
+      #1
+      out2 = (o == -4);
+      $display("DIV: %s %s", out1 ? "W" : "X", out2 ? "W" : "X");
 
       // SRL
-      funct3 = 3'h5; funct7 = 6'h00; 
+      f3 = 3'h5; f7 = 6'h00; 
       a = 16; b = 2;
-      out1 = (rd == 4);
+      #1
+      out1 = (o == 4);
       a = -16; b = 3;
-      out2 = (rd == 536870908); // Assuming the result should be 2^29 - 1
-      $display("SRL: %s %s", out1 ? "✔" : "X", out2 ? "✔" : "X");
+      #1
+      out2 = (o == 536870908); // Assuming the result should be 2^29 - 1
+      $display("SRL: %s %s", out1 ? "W" : "X", out2 ? "W" : "X");
 
       // SRA
-      funct3 = 3'h5; funct7 = 6'h20; 
+      f3 = 3'h5; f7 = 6'h20; 
       a = -16; b = 3;
-      out1 = (rd == -2);
+      #1
+      out1 = (o == -2);
       a = 16; b = 3;
-      out2 = (rd == 2);
-      $display("SRA: %s %s", out1 ? "✔" : "X", out2 ? "✔" : "X");
+      #1
+      out2 = (o == 2);
+      $display("SRA: %s %s", out1 ? "W" : "X", out2 ? "W" : "X");
 
       // DIVU
-      funct3 = 3'h5; funct7 = 6'h01;
+      f3 = 3'h5; f7 = 6'h01;
       a = 16; b = 3;
-      out1 = (rd == 5);
+      #1
+      out1 = (o == 5);
       a = -16; b = 3;
-      out2 = (rd == 536870909); // Assuming the result should be 2^29 - 2
-      $display("DIVU: %s %s", out1 ? "✔" : "X", out2 ? "✔" : "X");
+      #1
+      out2 = (o == 536870909); // Assuming the result should be 2^29 - 2
+      $display("DIVU: %s %s", out1 ? "W" : "X", out2 ? "W" : "X");
 
       // OR
-      funct3 = 3'h6; funct7 = 6'h00;
+      f3 = 3'h6; f7 = 6'h00;
       a = 5; b = 3;
-      out1 = (rd == 7);
+      #1
+      out1 = (o == 7);
       a = -5; b = -3;
-      out2 = (rd == -1);
-      $display("OR: %s %s", out1 ? "✔" : "X", out2 ? "✔" : "X");
+      #1
+      out2 = (o == -1);
+      $display("OR: %s %s", out1 ? "W" : "X", out2 ? "W" : "X");
 
       // REM
-      funct3 = 3'h6; funct7 = 6'h01;
+      f3 = 3'h6; f7 = 6'h01;
       a = 10; b = 3;
-      out1 = (rd == 1);
+      #1
+      out1 = (o == 1);
       a = -10; b = 3;
-      out2 = (rd == -1);
-      $display("REM: %s %s", out1 ? "✔" : "X", out2 ? "✔" : "X");
+      #1
+      out2 = (o == -1);
+      $display("REM: %s %s", out1 ? "W" : "X", out2 ? "W" : "X");
 
       // AND
-      funct3 = 3'h7; funct7 = 6'h00;
+      f3 = 3'h7; f7 = 6'h00;
       a = 5; b = 3;
-      out1 = (rd == 1);
+      #1
+      out1 = (o == 1);
       a = -5; b = -3;
-      out2 = (rd == -7);
-      $display("AND: %s %s", out1 ? "✔" : "X", out2 ? "✔" : "X");
+      #1
+      out2 = (o == -7);
+      $display("AND: %s %s", out1 ? "W" : "X", out2 ? "W" : "X");
 
       // REMU
-      funct3 = 3'h7; funct7 = 6'h01;
+      f3 = 3'h7; f7 = 6'h01;
       a = 10; b = 3;
-      out1 = (rd == 1);
+      #1
+      out1 = (o == 1);
       a = -10; b = 3;
-      out2 = (rd == 1);
-      $display("REMU: %s %s", out1 ? "✔" : "X", out2 ? "✔" : "X");
+      #1
+      out2 = (o == 1);
+      $display("REMU: %s %s", out1 ? "W" : "X", out2 ? "W" : "X");
     end
 endmodule
