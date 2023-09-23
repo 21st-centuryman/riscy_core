3a1a08bdbedf22f8a4c5a412e343abb3aecc1a06
calexanderberg <carlalexanderberg@gmail.com>
2023-09-23 08:58:52 +0200

    Fix: i prefer checkmarks

--- tests/cpu/alu_tb.sv
+++ tests/cpu/alu_tb.sv
@@ -20,7 +20,7 @@ module TB();
       a = -2; b = -4;
       #1
       out2 = (o == -6);
-      $display("ADD: %s %s", out1 ? "W" : "X", out2 ? "W" : "X");
+      $display("ADD: %s %s", out1 ? "✓" : "X", out2 ? "✓" : "X");
 
       // SUB
       f3 = 3'h0; f7 = 6'h20;
@@ -30,7 +30,7 @@ module TB();
       a = -2; b = -4;
       #1
       out2 = (o == 2);
-      $display("SUB: %s %s", out1 ? "W" : "X", out2 ? "W" : "X");
+      $display("SUB: %s %s", out1 ? "✓" : "X", out2 ? "✓" : "X");
 
       // MUL
       f3 = 3'h0; f7 = 6'h01;
@@ -40,7 +40,7 @@ module TB();
       a = -2; b = -4;
       #1
       out2 = (o == 8);
-      $display("MUL: %s %s", out1 ? "W" : "X", out2 ? "W" : "X");
+      $display("MUL: %s %s", out1 ? "✓" : "X", out2 ? "✓" : "X");
 
       // SLL
       f3 = 3'h1; f7 = 6'h00;
@@ -50,7 +50,7 @@ module TB();
       a = -2; b = 3;
       #1
       out2 = (o == -16);
-      $display("SLL: %s %s", out1 ? "W" : "X", out2 ? "W" : "X");
+      $display("SLL: %s %s", out1 ? "✓" : "X", out2 ? "✓" : "X");
 
       // MULH
       f3 = 3'h1; f7 = 6'h01;
@@ -60,7 +60,7 @@ module TB();
       a = -2; b = -4;
       #1
       out2 = (o == 0); // Assuming the result should be 0
-      $display("MULH: %s %s", out1 ? "W" : "X", out2 ? "W" : "X");
+      $display("MULH: %s %s", out1 ? "✓" : "X", out2 ? "✓" : "X");
 
       // SLT
       f3 = 3'h2; f7 = 6'h00;
@@ -70,7 +70,7 @@ module TB();
       a = -2; b = -4;
       #1
       out2 = (o == 0);
-      $display("SLT: %s %s", out1 ? "W" : "X", out2 ? "W" : "X");
+      $display("SLT: %s %s", out1 ? "✓" : "X", out2 ? "✓" : "X");
 
       // Test MULSU operation
       f3 = 3'h2; f7 = 6'h01; // MULSU
@@ -80,7 +80,7 @@ module TB();
       a = -2; b = -4;
       #1
       out2 = (o == 0); // Assuming the result should be 0
-      $display("MULSU: %s %s", out1 ? "W" : "X", out2 ? "W" : "X");
+      $display("MULSU: %s %s", out1 ? "✓" : "X", out2 ? "✓" : "X");
 
       // SLTU 
       f3 = 3'h3; f7 = 6'h00; 
@@ -90,7 +90,7 @@ module TB();
       a = -2; b = -4;
       #1
       out2 = (o == 0);
-      $display("SLTU: %s %s", out1 ? "W" : "X", out2 ? "W" : "X");
+      $display("SLTU: %s %s", out1 ? "✓" : "X", out2 ? "✓" : "X");
 
       // MULU
       f3 = 3'h3; f7 = 6'h01;
@@ -100,7 +100,7 @@ module TB();
       a = -2; b = -4;
       #1
       out2 = (o == 0); // Assuming the result should be 0
-      $display("MULU: %s %s", out1 ? "W" : "X", out2 ? "W" : "X");
+      $display("MULU: %s %s", out1 ? "✓" : "X", out2 ? "✓" : "X");
 
       // XOR
       f3 = 3'h4; f7 = 6'h00;
@@ -110,7 +110,7 @@ module TB();
       a = -2; b = -4;
       #1
       out2 = (o == -2);
-      $display("XOR: %s %s", out1 ? "W" : "X", out2 ? "W" : "X");
+      $display("XOR: %s %s", out1 ? "✓" : "X", out2 ? "✓" : "X");
 
       // DIV
       f3 = 3'h4; f7 = 6'h01;
@@ -120,7 +120,7 @@ module TB();
       a = -8; b = 2;
       #1
       out2 = (o == -4);
-      $display("DIV: %s %s", out1 ? "W" : "X", out2 ? "W" : "X");
+      $display("DIV: %s %s", out1 ? "✓" : "X", out2 ? "✓" : "X");
 
       // SRL
       f3 = 3'h5; f7 = 6'h00; 
@@ -130,7 +130,7 @@ module TB();
       a = -16; b = 3;
       #1
       out2 = (o == 536870908); // Assuming the result should be 2^29 - 1
-      $display("SRL: %s %s", out1 ? "W" : "X", out2 ? "W" : "X");
+      $display("SRL: %s %s", out1 ? "✓" : "X", out2 ? "✓" : "X");
 
       // SRA
       f3 = 3'h5; f7 = 6'h20; 
@@ -140,7 +140,7 @@ module TB();
       a = 16; b = 3;
       #1
       out2 = (o == 2);
-      $display("SRA: %s %s", out1 ? "W" : "X", out2 ? "W" : "X");
+      $display("SRA: %s %s", out1 ? "✓" : "X", out2 ? "✓" : "X");
 
       // DIVU
       f3 = 3'h5; f7 = 6'h01;
@@ -150,7 +150,7 @@ module TB();
       a = -16; b = 3;
       #1
       out2 = (o == 536870909); // Assuming the result should be 2^29 - 2
-      $display("DIVU: %s %s", out1 ? "W" : "X", out2 ? "W" : "X");
+      $display("DIVU: %s %s", out1 ? "✓" : "X", out2 ? "✓" : "X");
 
       // OR
       f3 = 3'h6; f7 = 6'h00;
@@ -160,7 +160,7 @@ module TB();
       a = -5; b = -3;
       #1
       out2 = (o == -1);
-      $display("OR: %s %s", out1 ? "W" : "X", out2 ? "W" : "X");
+      $display("OR: %s %s", out1 ? "✓" : "X", out2 ? "✓" : "X");
 
       // REM
       f3 = 3'h6; f7 = 6'h01;
@@ -170,7 +170,7 @@ module TB();
       a = -10; b = 3;
       #1
       out2 = (o == -1);
-      $display("REM: %s %s", out1 ? "W" : "X", out2 ? "W" : "X");
+      $display("REM: %s %s", out1 ? "✓" : "X", out2 ? "✓" : "X");
 
       // AND
       f3 = 3'h7; f7 = 6'h00;
@@ -180,7 +180,7 @@ module TB();
       a = -5; b = -3;
       #1
       out2 = (o == -7);
-      $display("AND: %s %s", out1 ? "W" : "X", out2 ? "W" : "X");
+      $display("AND: %s %s", out1 ? "✓" : "X", out2 ? "✓" : "X");
 
       // REMU
       f3 = 3'h7; f7 = 6'h01;
@@ -190,6 +190,6 @@ module TB();
       a = -10; b = 3;
       #1
       out2 = (o == 1);
-      $display("REMU: %s %s", out1 ? "W" : "X", out2 ? "W" : "X");
+      $display("REMU: %s %s", out1 ? "✓" : "X", out2 ? "✓" : "X");
     end
 endmodule
