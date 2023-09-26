04f53ed5b181da4ace8d5d489562aa80ea76acae
21st <60197499+21st-centuryman@users.noreply.github.com>
2023-09-26 15:01:40 +0200

    Add: update sv version

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,4 @@
 #cpanm --local-lib=~/perl5 local::lib && eval $(perl -I ~/perl5/lib/perl5/ -Mlocal::lib); cpanm Switch
-iverilog -o run -g2005-sv $1 $t2
+iverilog -o run -g2012 $1 $t2
 #vvp run
 #gtkwave sim.vcd
