0801c10e7d748de008113d326d2d542d2720700d
21st <60197499+21st-centuryman@users.noreply.github.com>
2023-09-26 10:12:07 +0200

    Fix; Small fixes

--- src/cpu/cpu.sv
+++ src/cpu/cpu.sv
@@ -13,37 +13,46 @@ endmodule
 /* ------------------------------ CONTROLLER ------------------------------ */
 module controller (
   input clk,
-  input[31:0] instr,
-  output [31:0] out,pc
+  input[31:0] instr, pc,
+  output [31:0] out,pc_out
 );
   always_ff @(posedge clk) begin : controller
-    case (instr[6:0])
-      7'b0110011: begin
+    case (instr[6:0]) // OP CODE
+      7'b0110011: begin : R-Type
         // Fetch the value of rs1, rs2, and the register we need to .rd
         alu #(.funct3(instr[14:12]), .funct7(instr[31:25]), .rs1(rs1), .rs2(rs2)) .rd(out);
-        pc += 4;
+        pc_out <= pc + 4;
       end
-      7'b0010011: begin
+      
+      7'b0010011: begin : I-Type
         // Fetch the value of rs1 and register rd
         assign logic [6:0] f7 = (instr[31:25] == 6'h00 || instr[31:25] == 6'h20) ?
           instr[31:25] : 6'h00;
-        assign logic [31:0] pc_out;
-        alu #(.funct3(instr[14:12]), .funct7(f7), .rs1(rs1), .rs2({21'b0, instr[31:19])}) .rd(pc_out);
-        pc <= pc_out;
+        alu #(.funct3(instr[14:12]), .funct7(f7), .rs1(rs1), .rs2({21'b0, instr[31:19])}) .rd(pc_out;);
       end
-      7'b0000011: begin
+              
+      7'b0000011: begin : 
         // Load type
+        pc_out <= pc + 4;
       end
-      7'b0100011: begin
+              
+      7'b0100011: begin : S-Type
         // S type
+        pc_out <= pc + 4;
       end
-      7'b1100011: begin
+              
+      7'b1100011: begin : B-Type
         // B type
+        pc_out <= pc + 4;
       end
-      7'b1101111: begin
-       jal #(.pc(pc), .imm(instr[31:12]), .pc_out(pc));
+              
+      7'b1101111: begin : jal
+        jal #(.pc(pc), .imm(instr[31:12]), .pc_out(pc_out));
       end      
-      7'01100111: // jalr
+              
+      7'01100111: begin : jalr
+        pc_out <= pc + 4;
+      end
     endcase
   end
 endmodule
@@ -62,9 +71,9 @@ module branch (
     case (funct3)
       3'h0: pc_out = (rs1 == rs2) ? pc + imm : pc + 4; // beq
       3'h1: pc_out = (rs1 != rs2) ? pc + imm : pc + 4; // bne
-      3'h4: pc_out = (rs1 < rs2) ? pc + imm : pc + 4; // blt
+      3'h4: pc_out = (rs1 < rs2) ? pc + imm : pc + 4;  // blt
       3'h5: pc_out = (rs1 >= rs2) ? pc + imm : pc + 4; // bge
-      3'h6: pc_out = (rs1 < rs2) ? pc + imm : pc + 4; // bltu
+      3'h6: pc_out = (rs1 < rs2) ? pc + imm : pc + 4;  // bltu
       3'h7: pc_out = (rs1 >= rs2) ? pc + imm : pc + 4; // bgeu
     endcase
   end
@@ -104,14 +113,14 @@ always_comb begin
 
     3'h2:  // SLT | MUILSU
       case (funct7)
-        6'h00: rd = (rs1 <  rs2);         // SLT
-        6'h01: rd = ((rs1 * rs2) >> 32);  // MULSU
+        6'h00: rd = rs1 <  rs2;         // SLT
+        6'h01: rd = (rs1 * rs2) >> 32;  // MULSU
       endcase
 
-    3'h3: // SLTU MULU
+    3'h3: // SLTU | MULU
       case (funct7)
         6'h00: rd = {31'b0, rs1 < rs2};   // SLTU
-        6'h01: rd = ((rs1 * rs2) >> 32);  // MULU
+        6'h01: rd = (rs1 * rs2) >> 32;    // MULU
       endcase
 
     3'h4: // XOR | DIV
@@ -122,9 +131,9 @@ always_comb begin
 
     3'h5: begin // SRL | SRA | DIVU
       case (funct7)
-        6'h00: rd = rs1 >> rs2;          // SRL
-        6'h20: rd = rs1 >>> rs2;         // SRA
-        6'h01: rd = rs1 / rs2;           // DIVU
+        6'h00: rd = rs1 >> rs2;  // SRL
+        6'h20: rd = rs1 >>> rs2; // SRA
+        6'h01: rd = rs1 / rs2;   // DIVU
       endcase
     end
 
@@ -136,8 +145,8 @@ always_comb begin
 
     3'h7: // AND | REMU
       case (funct7)
-        6'h00: rd = (rs1 & rs2);          // AND
-        6'h01: rd = ((rs1 * rs2) >> 32);  // REM
+        6'h00: rd = rs1 & rs2;          // AND
+        6'h01: rd = (rs1 * rs2) >> 32;  // REM
       endcase
   endcase
 end
