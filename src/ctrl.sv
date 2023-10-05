3ebe693f8976791e795aa29adf7680967820b6b6
calexanderberg <carlalexanderberg@gmail.com>
2023-10-05 12:34:34 +0200

    Add: controller. Need to fix errors

--- src/ctrl.sv
+++ src/ctrl.sv
@@ -1,20 +1,17 @@
-// This is really ugly, but I am blidnly following the harris and harris
-// guide. As soon as we have a working cpu we will worry about cleaning this
-// up
-module controller (
-  input zero,
+module ctrl (
   input [6:0] op,
   input [2:0] funct3,
   input funct7,
+  input zero,
   // OUTPUTS
-  output pcSrc, ResultSrc, MemWrite, ALUSrc, Immsrc, RegWrite
-  output [2:0] ALUControl
+  output reg pcSrc, ResultSrc, MemWrite, ALUSrc, Immsrc, RegWrite,
+  output reg [2:0] ALUControl
 );
-always_ff @(op) begin : main_decoder
-  logic branch; logic jump;
-  logic[1:0] ALUOp;
+always @(op or funct3 or funct7 or zero) begin // main_decoder
+  reg branch, jump;
+  reg [1:0] ALUOp;
   case (op)
-    7'b0000011: begin : lw
+    7'b0000011: begin // lw
       RegWrite = 1;
       Immsrc = 2'b00;
       ALUSrc = 1;
@@ -24,7 +21,7 @@ always_ff @(op) begin : main_decoder
       ALUOp = 2'b00;
       jump = 0;
     end
-    7'b0100011: begin : sw
+    7'b0100011: begin // sw
       RegWrite = 0;
       Immsrc = 2'b01;
       ALUSrc = 1;
@@ -33,7 +30,7 @@ always_ff @(op) begin : main_decoder
       ALUOp = 2'b00;
       jump = 0;
     end
-    7'b0110011: begin : r-type
+    7'b0110011: begin // r-type
       RegWrite = 1;
       ALUSrc = 0;
       MemWrite = 0;
@@ -42,7 +39,7 @@ always_ff @(op) begin : main_decoder
       ALUOp = 2'b10;
       jump = 0;
     end
-    7'b1100011: begin : beq
+    7'b1100011: begin // beq
       RegWrite = 0;
       Immsrc = 2'b10;
       ALUSrc = 0;
@@ -51,7 +48,7 @@ always_ff @(op) begin : main_decoder
       ALUOp = 2'b01;
       jump = 0;
     end
-    7'b0010011: begin : i-type ALU
+    7'b0010011: begin // i-type ALU
       RegWrite = 1;
       Immsrc = 2'b00;
       ALUSrc = 1;
@@ -61,7 +58,7 @@ always_ff @(op) begin : main_decoder
       ALUOp = 2'b10;
       jump = 0;
     end
-    7'b1101111: begin : jal
+    7'b1101111: begin // jal
       RegWrite = 1;
       Immsrc = 2'b11;
       MemWrite = 0;
@@ -70,23 +67,20 @@ always_ff @(op) begin : main_decoder
       jump = 1;
     end
   endcase
+  if (op != 7'b1101111) begin
+    case (ALUOp)
+      2'b00: ALUControl = 3'b000;
+      2'b01: ALUControl = 3'b001;
+      2'b10: begin
+        case (funct3)
+          3'b000: ALUControl = (funct7 == 3'b11) ? 3'b001 : 3'b000;
+          3'b010: ALUControl = 3'b101;
+          3'b110: ALUControl = 3'b011;
+          3'b111: ALUControl = 3'b010;
+        endcase
+      end
+    endcase
+  end
+  pcSrc = (jump | (branch & zero));
 end
-
-always_ff @(op or funct3 or funct7 or ALUOp) begin : alu_decoder
-  case (ALUOp)
-    2'b00: ALUControl = 3'b000;
-    2'b01: ALUControl = 3'b001;
-    2'b10: begin
-      case (funct3)
-        3'b000: ALUControl = ({op[5], funct7} == 2'b11) ? 3'b001 : 3'b000;
-        3'b010: ALUControl = 3'b101;
-        3'b110: ALUControl = 3'b011;
-        3'b111: ALUControl = 3'b010;
-      endcase
-    end
-  endcase
-end
-
-assign pcSrc = (jump | (branch & zero));
-
 endmodule
--- tests/ctrl_tb.sv
+++ tests/ctrl_tb.sv
@@ -1,43 +1,142 @@
 `timescale 10ns/1ns
 
-module alu_tb();
-  logic [31:0] a, b, out;
-  logic [2:0] ctrl;
-  logic z;
+module ctrl_tb();
+  logic [6:0] op;
+  logic [2:0] funct3;
+  logic funct7, zero;
+  logic [2:0] ALUControl;
+  logic pcSrc, ResultSrc, MemWrite, ALUSrc, Immsrc, RegWrite;
 
-  alu alu(.rs1(a), .rs2(b), .rd(out), .z(z), .ctrl(ctrl)); 
+  reg status;
+
+  ctrl ctrl(
+    .op(op), 
+    .zero(zero),
+    .funct3(funct3),
+    .funct7(funct7),
+    .pcSrc(pcSrc), 
+    .ResultSrc(ResultSrc), 
+    .MemWrite(MemWrite), 
+    .ALUSrc(ALUSrc), 
+    .Immsrc(Immsrc), 
+    .RegWrite(RegWrite),
+    .ALUControl(ALUControl)
+    );
 
   initial begin
       $dumpfile("sim.vcd"); 
-      $dumpvars(0, a, b, out, z, ctrl);
+      $dumpvars(
+        0,
+        op, 
+        funct3, 
+        funct7, 
+        ALUControl, 
+        pcSrc, 
+        ResultSrc, 
+        MemWrite, 
+        ALUSrc, 
+        Immsrc, 
+        RegWrite
+      );
 
+      zero = 0;
+      status = 0;
+
+      // -------------------------------
+      //        MAIN DECORDER
+      // -------------------------------
+      op = 7'b0000011; // lw
       #1
-      ctrl = 3'b000; // add
-      a = 20; b = 30;
-      assert(out = (a + b)) else $display("ADD: is broken");
+      assert(
+        RegWrite == 1 &&
+        Immsrc == 2'b00 &&
+        ALUSrc == 1 &&
+        MemWrite == 0 &&
+        ResultSrc == 2'b01 &&
+        pcSrc == 0 && // Set expected_pcSrc to the expected value
+        ALUControl == 3'b000
+      ) else begin
+        $error("lw: is broken"); 
+        status = 1;
+      end
+
+      op = 7'b0100011; // sw
+      zero = 0;
+      #1
+      assert(
+        RegWrite == 0 &&
+        Immsrc == 2'b01 &&
+        ALUSrc == 1 &&
+        MemWrite == 1 &&
+        pcSrc == 0 &&
+        ALUControl == 3'b000
+      ) else begin
+        $error("sw: is broken"); 
+        status = 1;
+      end
+
+      op = 7'b1101111; // jal
+      zero = 0;
+      #1
+      assert(
+        RegWrite == 1 &&
+        Immsrc == 2'b11 &&
+        MemWrite == 0 &&
+        ResultSrc == 2'b10 &&
+        pcSrc == 1
+      ) else begin
+        $error("jal: is broken");
+        status = 1;
+      end
+
+      op = 7'b1100011; // beq
+      zero = 1;
+      #1
+      assert(
+        RegWrite == 0 &&
+        Immsrc == 2'b10 &&
+        ALUSrc == 0 &&
+        MemWrite == 0 &&
+        pcSrc == 1 &&
+        ALUControl == 3'b001
+      ) else begin
+        $error("beq: is broken");
+        status = 1;
+      end
+
+      op = 7'b0010011; // addi
+      funct3 = 1'h0;
+      #1
+      assert(
+        RegWrite == 1 &&
+        Immsrc == 2'b00 &&
+        ALUSrc == 1 &&
+        MemWrite == 0 &&
+        ResultSrc == 2'b00 &&
+        pcSrc == 0 &&
+        ALUControl == 3'b000
+      ) else begin
+        $error("i-type alu 1: is broken"); 
+        status = 1;
+      end
+
+      op = 7'b0000011; // r-type
+      #1
+      assert(
+        RegWrite == 1 &&
+        Immsrc == 2'b00 &&
+        ALUSrc == 1 &&
+        MemWrite == 0 &&
+        ResultSrc == 2'b01 &&
+        pcSrc == 0 &&
+        ALUControl == 3'b000
+      ) else begin
+        $error("r-type: is broken");
+        status = 1;
+      end
 
       #1;
-      ctrl = 3'b001; // sub
-      a = 20; b = 30;
-      assert(out = (a - b)) else $display("SUB: is broken");
 
-      #1;
-      ctrl = 3'b010; // and
-      a = 20; b = 30;
-      assert(out = (a & b)) else $display("AND: is broken");
-
-      #1;
-      ctrl = 3'b011; // or
-      a = 20; b = 30;
-      assert(out = (a | b)) else $display("OR: is broken");
-
-      #1;
-      ctrl = 3'b101; // slt
-      a = 20; b = 30;
-      assert(out = (a < b)) else $display("SLT: is broken");
-
-      #1;
-
-      $finish;
+      $stop(status);
     end
 endmodule
