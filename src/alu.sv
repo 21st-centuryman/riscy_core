bd7fef30c2292dc6153478a49e9031a43961ac3d
calexanderberg <carlalexanderberg@gmail.com>
2024-05-14 23:42:07 +0200

    Fix: alu

--- src/alu.sv
+++ src/alu.sv
@@ -11,11 +11,11 @@ module alu (
   always_ff @(posedge clk) begin
     case (funct3)
       3'b000:  rd <= !funct7[6] ? (rs1 + rs2) : (rs1 - rs2);  // ADD | SUB
-      //3'b001: rd <= rs1 << rs2;  // SLL
-      //3'b010: rd <= (rs1 < rs2) ? 1 : 0;  // SLT
-      //3'b011: rd <= (rs1 < rs2) ? 1 : 0;  // SLTU
+      3'b001:  rd <= rs1 << rs2;  // SLL
+      3'b010:  rd <= (rs1 < rs2) ? 1 : 0;  // SLT
+      3'b011:  rd <= (rs1 < rs2) ? {31'b0, 1'b1} : {31'b0, 1'b0};  // SLTU
       3'b100:  rd <= (rs1 ^ rs2);  // XOR
-      //3'b101: rd <= !tmp ? rs1 >> rs2 : rs1 >> rs2;  // SRL | SRA
+      3'b101:  rd <= !funct7[6] ? (rs1 >> rs2) : (rs1 >>> rs2);  // SRL | SRA
       3'b110:  rd <= (rs1 | rs2);  // OR
       3'b111:  rd <= (rs1 & rs2);  // AND
       default;
@@ -24,8 +24,3 @@ module alu (
   assign z = (rs1 - rs2) == 0;
 endmodule
 
-
-
-// -----------------------
-// FIX FINISH THIS AND FIX THE TEST
-// -----------------------
