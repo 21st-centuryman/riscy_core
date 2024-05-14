65279c4ae52d6935461405a5da902e45ba3c5424
calexanderberg <carlalexanderberg@gmail.com>
2024-05-14 23:58:30 +0200

    Add: updated tests

--- tests/alu_tb.sv
+++ tests/alu_tb.sv
@@ -2,9 +2,10 @@ module alu_tb ();
   logic [31:0] a, b, out;
   logic [2:0] funct3;
   logic [6:0] funct7;
-  logic z, clk;
+  logic z;
 
   reg status;
+  reg clk;
 
   alu alu (
       .clk(clk),
@@ -26,73 +27,73 @@ module alu_tb ();
     $dumpvars(0, a, b, out, z, funct3, funct7);
     clk = 0;
 
-    #1 funct3 = 3'b000;  // ADD
+    funct3 = 3'b000;  // ADD
     funct7 = 7'h00;
     a = 20;
     b = 30;
-    #1
-    assert (out == (a - b)) else $error("ADD: is broken");
+    assert (out == (a - b))
+    else $error("ADD: is broken");
 
-    #1 funct3 = 3'b000;  // SUB
+    funct3 = 3'b000;  // SUB
     funct7 = 7'h20;
     a = 8;
     b = 3;
-    #1
-    assert (out == (a - b)) else $error("SUB: is broken");
+    assert (out == (a - b))
+    else $error("SUB: is broken");
 
-    //funct3 = 3'b001;  // SLL FIX
-    //funct7 = 7'h00;
-    //a = 8;
-    //b = 3;
-    //assert (out == (a - b))
-    //else $error("SLL: is broken");
-
-    // funct3 = 3'b010;  // SLT FIX
-    // funct7 = 7'h00;
-    // a = 8;
-    // b = 3;
-    // assert (out == (a - b))
-    // else $error("SLT: is broken");
-
-    // funct3 = 3'b010;  // SLTU FIX
-    // funct7 = 7'h00;
-    // a = 8;
-    // b = 3;
-    // assert (out == (a - b))
-    // else $error("SLTU: is broken");
-
-    #1 funct3 = 3'b010;  // XOR FIX
+    funct3 = 3'b001;  // SLL
     funct7 = 7'h00;
     a = 8;
     b = 3;
-    #1
-    assert (out == (a ^ b)) else $error("XOR: is broken");
+    assert (out == (a << b))
+    else $error("SLL: is broken");
 
-    // funct3 = 3'b101;  // SRL FIX
-    // funct7 = 7'h00;
-    // a = 8;
-    // b = 3;
-    // assert (out == (a - b))
-    // else $error("SRL: is broken");
+    funct3 = 3'b010;  // SLT
+    funct7 = 7'h00;
+    a = 8;
+    b = 3;
+    assert (out == (a < b ? 1 : 0))
+    else $error("SLT: is broken");
 
-    // funct3 = 3'b101;  // SRA FIX
-    // funct7 = 7'h20;
-    // a = 8;
-    // b = 3;
-    // assert (out == (a - b))
-    // else $error("SRA: is broken");
+    funct3 = 3'b010;  // SLTU
+    funct7 = 7'h00;
+    a = 8;
+    b = 3;
+    assert (out == (a < b ? 1 : 0))
+    else $error("SLTU: is broken");
 
-    #1 funct3 = 3'b110;  // OR
+    funct3 = 3'b010;  // XOR
+    funct7 = 7'h00;
+    a = 8;
+    b = 3;
+    assert (out == (a ^ b))
+    else $error("XOR: is broken");
+
+    funct3 = 3'b101;  // SRL
+    funct7 = 7'h00;
+    a = 8;
+    b = 3;
+    assert (out == (a >> b))
+    else $error("SRL: is broken");
+
+    funct3 = 3'b101;  // SRA
+    funct7 = 7'h20;
+    a = 8;
+    b = 3;
+    assert (out == (a >> b))
+    else $error("SRA: is broken");
+
+    funct3 = 3'b110;  // OR
     a = 20;
     b = 30;
-    #1
-    assert (out == (a | b)) else $error("OR: is broken");
+    assert (out == (a | b))
+    else $error("OR: is broken");
 
-    #1 funct3 = 3'b111;  // AND
+    funct3 = 3'b111;  // AND
     a = 20;
     b = 30;
-    #1
-    assert (out == (a & b)) else $error("AND: is broken");
+    assert (out == (a & b))
+    else $error("AND: is broken");
 
 
     // -----------------------------
