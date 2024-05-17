623bc0c5966aefcb3a5fd9c7d3e5308e421a991b
calexanderberg <carlalexanderberg@gmail.com>
2024-05-17 11:02:37 +0200

    Revert changes

--- src/alu.sv
+++ src/alu.sv
@@ -10,12 +10,12 @@ module alu (
 );
   always_ff @(posedge clk) begin
     case (funct3)
-      3'b000:  rd <= (funct7 == 0) ? (rs1 + rs2) : (rs1 - rs2);  // ADD | SUB
+      3'b000:  rd <= !funct7 ? (rs1 + rs2) : (rs1 - rs2);  // ADD | SUB
       3'b001:  rd <= rs1 << rs2;  // SLL
       3'b010:  rd <= (rs1 < rs2) ? 1 : 0;  // SLT
       3'b011:  rd <= (rs1 < rs2) ? {31'b0, 1'b1} : {31'b0, 1'b0};  // SLTU
       3'b100:  rd <= (rs1 ^ rs2);  // XOR
-      3'b101:  rd <= (funct7 == 0) ? (rs1 >> rs2) : (rs1 >>> rs2);  // SRL | SRA
+      3'b101:  rd <= !funct7 ? (rs1 >> rs2) : (rs1 >>> rs2);  // SRL | SRA
       3'b110:  rd <= (rs1 | rs2);  // OR
       3'b111:  rd <= (rs1 & rs2);  // AND
       default;
