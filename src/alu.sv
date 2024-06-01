214f51ecf4e44f0cea7c93998a54dcbe3a77da08
calexanderberg <carlalexanderberg@gmail.com>
2024-06-01 22:41:12 +0200

    Add: nicer ALU code

--- src/alu.sv
+++ src/alu.sv
@@ -10,15 +10,19 @@ module alu (
     output z
 );
   always_ff @(posedge clk) begin
-    case (funct3)
-      3'b000:  rd <= !funct7 ? (rs1 + rs2) : (rs1 - rs2);  // ADD | SUB
-      3'b001:  rd <= rs1 << rs2;  // SLL
-      3'b010:  rd <= (rs1 < rs2) ? 1 : 0;  // SLT
-      3'b011:  rd <= (rs1 < rs2) ? {31'b0, 1'b1} : {31'b0, 1'b0};  // SLTU
-      3'b100:  rd <= (rs1 ^ rs2);  // XOR
-      3'b101:  rd <= !funct7 ? (rs1 >> rs2) : (rs1 >>> rs2);  // SRL | SRA
-      3'b110:  rd <= (rs1 | rs2);  // OR
-      3'b111:  rd <= (rs1 & rs2);  // AND
+    case ({
+      funct7, funct3
+    })
+      4'b0000: rd <= (rs1 + rs2);  // ADD
+      4'b1000: rd <= (rs1 - rs2);  // SUB
+      4'b0001: rd <= rs1 << rs2;  // SLL
+      4'b0010: rd <= (rs1 < rs2) ? 1 : 0;  // SLT
+      4'b0011: rd <= {31'b0, rs1 < rs2};  // SLTU
+      4'b0100: rd <= (rs1 ^ rs2);  // XOR
+      4'b0101: rd <= (rs1 >> rs2);  // SRL
+      4'b1101: rd <= (rs1 >>> rs2);  // SRA
+      4'b0110: rd <= (rs1 | rs2);  // OR
+      4'b0111: rd <= (rs1 & rs2);  // AND
       default;
     endcase
   end
